9d08d6d1e22c11ef3c5688fcb8842eb00175c553
abouzek <alan.bouzek@gmail.com>
2014-01-29 19:36:07 -0600

    move bot settings to json file

--- mexibot.py
+++ mexibot.py
@@ -6,26 +6,26 @@ https://github.com/abouzek/reddit_scripts
 
 """
 
-import praw, goslate, logging, re
+import praw, goslate, json
 from collections import deque
 from time import sleep
 
 def check(comment):
     size = len(comment.body)
-    return (size > MIN_LENGTH and size < MAX_LENGTH
-        and comment.author.name != USER
-        and any(word in comment.body for word in KEYWORDS))
+    return (size > settings['min_length'] and size < settings['max_length']
+        and comment.author.name != settings['username']
+        and any(word in comment.body for word in set(settings['keywords'])))
 
 def action(comment):
-    print "Replying to comment: " + comment.body
+    print "Replying to comment: " + comment.body + "\n"
     text = gs.translate(comment.body, 'es')
-    text += SIGNATURE
+    text += settings['signature']
     comment.reply(text)
 
 def run_bot():
     cache = deque(maxlen=200)
     count = 0
-    for s in SUBREDDIT_NAMES:
+    for s in settings['subreddit_names']:
         print "Running on subreddit '" + s + "'..."
         comments = r.get_comments(s, limit=None)
         for c in comments:
@@ -45,17 +45,10 @@ def run_bot():
                     sleep(30)
     print "Finished. Replied to " + str(count) + " comments."
 
-USER = "mexibot"
-PASS = "tacotaquitobanana"
-USER_AGENT = "/u/mexibot"
-MIN_LENGTH = 500
-MAX_LENGTH = 5000
-SIGNATURE = "\n\n\n*-- Viva la mexibot! Translated because you used a particularly* **spicy** *word.*"
-SUBREDDIT_NAMES = ['all']
-KEYWORDS = set(['taco', 'burrito', 'queso', 'taquito', 'chaquita', 'chalupa', 'horchata', 'tortilla', 'cinco de mayo', 'mexican', 'chihuahua'])
-
-r = praw.Reddit(USER_AGENT)
-r.login(USER, PASS)
+settings_file = open('settings.json', 'r')
+settings = json.load(settings_file)
+r = praw.Reddit(settings['user_agent'])
+r.login(settings['username'], settings['password'])
 gs = goslate.Goslate()
 run_bot()
 
